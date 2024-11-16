14e45e19c987276081c10b708ede699279477a67
Maks <myakubovskiy2005@gmail.com>
2024-11-16 19:39:00 +0200

    v2

--- src/main/java/carrental/carrental_b/controllers/AuthController.java
+++ src/main/java/carrental/carrental_b/controllers/AuthController.java
@@ -48,7 +48,7 @@ public class AuthController {
 
     @PostMapping("/registration")
     public ResponseEntity<?> registerUser(@RequestBody RegisterDto registerDto) {
-        System.out.println(registerDto);
+        System.out.println("1");
         Map<String, String> errors = new HashMap<>();
 
         if (userService.isUsernameAlreadyTaken(registerDto.getUsername())) {
